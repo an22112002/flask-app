91f7d9dbab04b1390ef9f0a88b7738525f70b276
an22112002 <83687810+an22112002@users.noreply.github.com>
2023-11-25 17:19:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
+def hello():
+    return 'Hello from my Python app on Railway!'
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True)
