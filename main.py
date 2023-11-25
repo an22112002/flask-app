173b1d03e192b82f463e5cb67109a253ca9b5a8a
an22112002 <83687810+an22112002@users.noreply.github.com>
2023-11-25 17:31:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello from my Python app on Railway!'
+    return 'Hello from my Python app this is admin!'
 
 if __name__ == '__main__':
     app.run(debug=True)
